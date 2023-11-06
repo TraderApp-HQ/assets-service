8778f4efc21da4e5c8cb0ef2fe8702b4910154c3
ekidhaja <i@is-MacBook-Pro.local>
2023-11-06 07:44:44 +0100

    .

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -1,14 +1,14 @@
 import request from "supertest";
 
-const app = "http://localhost:8000";
+const app = "http://localhost:8080";
 
 describe("Testing API endpoints", () => {
-    describe("Pinging server", () => {
-        it("should respong with pong", async () => {
-            const res = await request(app).get("/api/v1/ping/").send();
+	describe("Pinging server", () => {
+		it("should respong with pong", async () => {
+			const res = await request(app).get("/ping").send();
 
-            expect(res.status).toEqual(200);
-            expect(res.body.message).toBe("pong");
-        })
-    })
+			expect(res.status).toEqual(200);
+			expect(res.body.message).toBe("pong");
+		});
+	});
 });

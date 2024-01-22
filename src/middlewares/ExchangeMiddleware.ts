4eb99607e9b85bdfa896d9c0e43a029ba2d5724d
Chinese CHIKKI <112833476+ChineseChikki@users.noreply.github.com>
2024-01-22 10:04:42 +0100

    added exchanges and Id handler (#29)
    * added exchanges and Id handler
    
    * implemented correction made
    
    * fix some corrections pointed out

---
+++ src/middlewares/ExchangeMiddleware.ts
@@ -0,0 +1,43 @@
+import { Request, Response, NextFunction } from "express";
+import Joi from "joi";
+import { checkAdmin } from "../utils/tokens";
+
+// A function to validate request to get all Exchange
+export async function validateExchangesRequest(req: Request, _res: Response, next: NextFunction) {
+	try {
+		// check accessToken and admin role
+		await checkAdmin(req);
+		next();
+	} catch (err: any) {
+		next(err);
+	}
+}
+
+// A function to validate request to get an Exchange by its ID
+export async function validateExchangeRequest(req: Request, _res: Response, next: NextFunction) {
+	// get id from request params and convert it to integer
+	const id = Number(req.params.id);
+
+	try {
+		// Joi schema to validate id req param
+		const schema = Joi.object({
+			id: Joi.number().required().label("Exchange ID"),
+		});
+
+		// validate id param
+		const { error } = schema.validate({ id });
+
+		if (error) {
+			// strip string of double quotes
+			error.message = error.message.replace(/\"/g, "");
+			next(error);
+			return;
+		}
+
+		// check accessToken and admin role
+		await checkAdmin(req);
+		next();
+	} catch (err: any) {
+		next(err);
+	}
+}

23c16c3d208aabd3c6ebe8ab3c7ec1f87bbba6eb
Spiff Jekey-Green <spiffjekeygreen@gmail.com>
2024-08-29 14:59:39 +0100

    docs: update docs fofr signals endpoint (active & history) (#46)

--- src/documentation/signal/index.ts
+++ src/documentation/signal/index.ts
@@ -1,4 +1,6 @@
 export * from "./createSignal.docs";
 export * from "./getSignal.docs";
+export * from "./getActiveSignal.docs";
+export * from "./getInActiveSignal.docs";
 export * from "./getSignalById.docs";
 export * from "./updateSignalById.docs";

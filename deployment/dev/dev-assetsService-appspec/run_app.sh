019aeee54cba9cb0235e666445912c2fc7c1ab97
Ndife Uchenna <32971118+Ndife@users.noreply.github.com>
2024-09-26 14:06:56 +0100

    update deploy scirpt

--- deployment/dev/dev-assetsService-appspec/run_app.sh
+++ deployment/dev/dev-assetsService-appspec/run_app.sh
@@ -14,4 +14,4 @@ aws ecr get-login-password --region eu-west-1 | sudo docker login --username AWS
 sudo docker pull 575439814610.dkr.ecr.eu-west-1.amazonaws.com/$ENV-$SERVICE-service:latest
 
 # Run the newly pulled image
-sudo docker run -d -p 80:8080 -e NODE_ENV=development -e SERVICE=$ENV-$SERVICEService --log-driver=awslogs --log-opt awslogs-region=eu-west-1 --log-opt awslogs-group=/aws/ec2/$ENV-$SERVICEService --log-opt awslogs-create-group=true 575439814610.dkr.ecr.eu-west-1.amazonaws.com/$ENV-$SERVICE-service:latest
+sudo docker run -d -p 80:8080 -e NODE_ENV=development -e SERVICE=dev-assetsService --log-driver=awslogs --log-opt awslogs-region=eu-west-1 --log-opt awslogs-group=/aws/ec2/dev-assetsService --log-opt awslogs-create-group=true 575439814610.dkr.ecr.eu-west-1.amazonaws.com/$ENV-$SERVICE-service:latest
